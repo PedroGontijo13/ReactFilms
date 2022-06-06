b8a0f1d4dfa1981f4423f23ec386c5d7ce06f9e9
PedroGont <pedrotolentino2002@gmail.com>
2022-06-05 19:45:13 -0700

    AvaliationsOK

--- src/Components/Avaliation.tsx
+++ src/Components/Avaliation.tsx
@@ -3,46 +3,44 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 export default function Avaliations() {
   return (
     <Container>
-        <Row>
-            <Col 
-          style={{marginTop: "10px"}}>
-                <h1>Avaliations:</h1>
-            </Col>
-        </Row>
       <Row>
-        <Col style={{marginTop: '10px', marginBottom: '10px'}} md={4}>
-          <Card bg="secondary">
+        <Col style={{ marginTop: "10px" }}>
+          <h1>Avaliations:</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ marginTop: "10px", marginBottom: "10px" }} md={4}>
+          <Card style={{ height: "100%", padding: "10px" }} bg="secondary">
             <Card.Body>
               <Card.Title>Pedro Tolentino</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                The page structure is very good, I just thought it lacked more
+                movies.
               </Card.Text>
-                ★★★☆☆
+              ★★★☆☆
             </Card.Body>
           </Card>
         </Col>
-        <Col style={{marginTop: '10px', marginBottom: '10px'}} md={4}>
-          <Card bg="secondary">
+        <Col style={{ marginTop: "10px", marginBottom: "10px" }} md={4}>
+          <Card style={{ height: "100%", padding: "10px" }} bg="secondary">
             <Card.Body>
-              <Card.Title>Pedro Tolentino</Card.Title>
+              <Card.Title>Carlos Gontijo</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                Great portal, game of very light colors, I liked it a lot.
               </Card.Text>
-                ★★★☆☆
+              ★★★★★
             </Card.Body>
           </Card>
         </Col>
-        <Col style={{marginTop: '10px', marginBottom: '10px'}} md={4}>
-          <Card bg="secondary">
+        <Col style={{ marginTop: "10px", marginBottom: "10px" }} md={4}>
+          <Card style={{ height: "100%", padding: "10px" }} bg="secondary">
             <Card.Body>
-              <Card.Title>Pedro Tolentino</Card.Title>
+              <Card.Title>Maria Eduarda</Card.Title>
               <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
+                I didn't really like having just a few fight movies on the
+                banner.
               </Card.Text>
-                ★★★☆☆
+              ★★★☆☆
             </Card.Body>
           </Card>
         </Col>
