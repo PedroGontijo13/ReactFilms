d20ab7928886781e481d552b284ad693d48da403
Pedro Gontijo <70620630+PedroGontijo13@users.noreply.github.com>
2023-03-17 23:28:27 -0700

    ✅

--- src/Pages/AllMovies.tsx
+++ src/Pages/AllMovies.tsx
@@ -1,6 +1,3 @@
-import AboutTheProject from "../Components/AbouTheProject";
-import AboutMe from "../Components/AboutMe";
-import Banner from "../Components/Banner";
 import Footer from "../Components/Footer";
 import MovieList from "../Components/MovieList";
 
